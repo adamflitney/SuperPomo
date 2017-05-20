b047fc10558e94989fcf1f4706b042925c3428de
adamflitney <adam.flitney@gmail.com>
2017-05-20 15:32:43 +0100

    basic functionality done

--- SuperPomo.js
+++ SuperPomo.js
@@ -1,14 +1,19 @@
 var timer = document.getElementById("timer");
-var toggleTimerButton = document.getElementById("toggleTimer");
+var toggleWorkTimerButton = document.getElementById("toggleWorkTimer");
+var toggleBreakTimerButton = document.getElementById("toggleBreakTimer");
 var resetButton = document.getElementById("resetTimer");
-var minutes = 24;
-var seconds = 59;
-var timerPaused = true;
+var message = document.getElementById("inspirationalMessage");
+var minutes = 25;
+var seconds = 0;
+var minutesDisplay = document.getElementById("minutes");
+var secondsDisplay = document.getElementById("seconds");
+var workTimerPaused = true;
+var breakTimerPaused = true;
 
 var x = setInterval(countdown, 1000);
 
 function countdown(){
-    if(!timerPaused){
+    if(!workTimerPaused || !breakTimerPaused ){
         //alert("timer not paused");
     if(seconds > 0){
         //alert("seconds > 0");
@@ -17,34 +22,100 @@ function countdown(){
         //alert("minutes > 0");
         minutes--;
         seconds = 59;
+    } else{
+        timerFinished();
     }
-    timer.textContent = formatTime(minutes, seconds);
+    displayTime(minutes, seconds);
 }
 
 }
 
-function formatTime(minutes, seconds){
+function displayTime(minutes, seconds){
     var minutesStr, secondsStr;
     minutesStr = minutes < 10 ? "0" + minutes : minutes;
     secondsStr = seconds < 10 ? "0" + seconds : seconds;
-    return minutesStr + ":" + secondsStr;
+    minutesDisplay.textContent = minutesStr;
+    secondsDisplay.textContent = secondsStr;
 }
 
 //timerPaused = false;
 
-toggleTimerButton.addEventListener("click", function(){
-    if(timerPaused){
-        toggleTimerButton.textContent = "Stop Timer";
-        timerPaused = false;
+toggleWorkTimerButton.addEventListener("click", function(){
+    if(workTimerPaused){
+        stopBreakTimer(); 
+        startWorkTimer();
     } else {
-        toggleTimerButton.textContent = "Start Timer";
-        timerPaused = true;
+        stopWorkTimer();
+    }
+});
+
+toggleBreakTimerButton.addEventListener("click", function(){
+    if(breakTimerPaused){
+        stopWorkTimer();
+        startBreakTimer();
+    } else {
+        stopBreakTimer();
     }
 });
 
 resetButton.addEventListener("click", function(){
+    toggleWorkTimerButton.classList.remove("bigger");
+    toggleBreakTimerButton.classList.remove("bigger");
+    stopWorkTimer();
+    stopBreakTimer();
     minutes = 25;
     seconds = 0;
-    timerPaused = true;
-    timer.textContent = formatTime(minutes, seconds);
-})
+    displayTime(minutes, seconds);
+    message.textContent = "Let's do some work!";
+});
+
+
+function stopWorkTimer() {
+    toggleWorkTimerButton.textContent = "Start Work";
+    workTimerPaused = true;
+    toggleWorkTimerButton.classList.remove("stop");
+    toggleWorkTimerButton.classList.add("start");
+}
+
+function startWorkTimer() {
+    toggleWorkTimerButton.classList.remove("bigger");
+    toggleBreakTimerButton.classList.remove("bigger");
+    minutes = 1;
+    seconds = 0;
+    displayTime(minutes, seconds);
+    toggleWorkTimerButton.textContent = "Stop Work";
+    workTimerPaused = false;
+    toggleWorkTimerButton.classList.remove("start");
+    toggleWorkTimerButton.classList.add("stop");
+    message.textContent = "Working!";
+}
+
+function stopBreakTimer() {
+    toggleBreakTimerButton.textContent = "Start Break";
+    breakTimerPaused = true;
+    toggleBreakTimerButton.classList.remove("stop");
+    toggleBreakTimerButton.classList.add("start");
+}
+
+function startBreakTimer() {
+    toggleWorkTimerButton.classList.remove("bigger");
+    toggleBreakTimerButton.classList.remove("bigger");
+    minutes = 1;
+    seconds = 0;
+    displayTime(minutes, seconds);
+    toggleBreakTimerButton.textContent = "Stop Break";
+    breakTimerPaused = false;
+    toggleBreakTimerButton.classList.remove("start");
+    toggleBreakTimerButton.classList.add("stop");
+    message.textContent = "Resting!";
+}
+
+function timerFinished(){
+    if(!workTimerPaused){
+        toggleBreakTimerButton.classList.add("bigger");
+        message.textContent = "Take a break!"
+    } else if(!breakTimerPaused){
+        toggleWorkTimerButton.classList.add("bigger");
+        message.textContent = "Back to work!"
+    }
+}
